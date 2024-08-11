f1d53bb89cd88899c7748328c40431f5e9a8d40b
joanhu716 <1442885996@qq.com>
2024-08-11 20:32:33 +0800

    Site updated: 2024-08-11 20:32:33

--- js/baidutongji.js
+++ js/baidutongji.js
@@ -2,7 +2,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?9e4e5344f9627267f5808dcb1ec344d9";
+  hm.src = "https://hm.baidu.com/hm.js?6fff6f41e12d71dc84e8fc68e5b9064c";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
